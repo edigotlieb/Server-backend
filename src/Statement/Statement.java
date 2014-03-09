c3692ca3f99e08c78fa9a5f1ac3ffb6164c616d9
T7639192 <T7639192@tlp-t-133.talpiot.huji.ac.il>
2014-03-10 01:16:17 +0200

    Statement1

---
+++ src/Statement/Statement.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Statement;
+
+/**
+ *
+ * @author T7639192
+ */
+public class Statement {
+	
+}

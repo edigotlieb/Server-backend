20baef57cd8e392cc6e01cc002b2da55c5dfeeb2
Edi Gotlieb <edi.gotlieb@gmail.com>
2014-03-10 22:43:57 +0200

    stuff

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+import SQL.PreparedStatements.PreparedStatementStrings;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PreparedStatementStrings pss = PreparedStatementStrings.getInstance();
+        System.out.println(pss.getSQL("getAllUserInfo"));
+    }
+    
+}
---
+++ src/SQL/PreparedStatements/PreparedStatementStrings.java
@@ -0,0 +1,56 @@
+/** 
+ * FILE : PreparedStatementStringas.java
+ * AUTHORS : Erez Gotlieb    
+ * DESCRIPTION : 
+ */ 
+
+package SQL.PreparedStatements;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PreparedStatementStrings {
+
+    private static HashMap<String,String> fileNames;
+    
+    static {
+        fileNames.put("getAllUserInfo", "getUser.sql");
+    }
+    
+    private HashMap<String,String> PreparedSql;
+    
+    private static PreparedStatementStrings instance = null;
+    
+    public static PreparedStatementStrings getInstance() {
+         if(instance != null) {
+             return instance;
+         } else {
+             return (instance = new PreparedStatementStrings());
+         }
+    }    
+    
+    private PreparedStatementStrings() {
+        readFiles();
+    }
+
+    private void readFiles() {
+        for(String key : fileNames.keySet()) {
+            try {
+                PreparedSql.put(key, new String(Files.readAllBytes(Paths.get("/preparedSQL/"+fileNames.get(key)))));
+            } catch (IOException ex) {
+                Logger.getLogger(PreparedStatementStrings.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public String getSQL(String key) {
+        return this.PreparedSql.get(key);
+    }
+    
+    
+    
+}

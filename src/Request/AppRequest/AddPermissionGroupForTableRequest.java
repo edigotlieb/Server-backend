8da76267ce3a7dfd25cfe0a05972dc312800fda7
T7639192 <T7639192@tlp-t-133.talpiot.huji.ac.il>
2014-04-02 21:56:59 +0300

    Validation stuff almost finishied

--- src/Request/AppRequest/AddPermissionGroupForTableRequest.java
+++ src/Request/AppRequest/AddPermissionGroupForTableRequest.java
@@ -51,10 +51,6 @@ public class AddPermissionGroupForTableRequest extends AppRequest{
 			throw new ValidationException(11);
 		}
 		
-		if(!this.creds.isMasterApplication()){
-			throw new ValidationException(13);
-		}
-		
         if(!this.creds.isAdminApp()){
 			throw new ValidationException(6);
 		}

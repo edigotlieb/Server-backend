9a7154e27903a14612431feba1b90dcb018d405c
T7639192 <T7639192@tlp-t-133.talpiot.huji.ac.il>
2014-04-11 00:20:40 +0300

    some bug fixed

--- src/Request/UserRequest/UserSelectRequest.java
+++ src/Request/UserRequest/UserSelectRequest.java
@@ -47,7 +47,7 @@ public class UserSelectRequest extends UserRequest {
 		final String usernameField = "USERNAME";
 		List<String> userCols = Utils.getColNames(sqlExc, userTable);
 		userCols.remove(passwordField);
-		Statement whereNoAnon = new AndStatement(this.where, new RelStatement(usernameField, Credentials.anonymous, "!="));
+		Statement whereNoAnon = new AndStatement(this.where, new RelStatement(usernameField, "'" + Credentials.anonymous + "'", "!="));
 		return sqlExc.executeDynamicStatementQry(SqlQueryGenerator.select(userCols, userTable, whereNoAnon));
 	}
 }

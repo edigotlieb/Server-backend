ab811dd7f5e11aa2393324ec9ec125d4fac8979d
T7639192 <T7639192@tlp-t-133.talpiot.huji.ac.il>
2014-04-15 18:52:23 +0300

    guytzman is annoying

--- src/Request/AppRequest/RemovePermissionGroupForTableRequest.java
+++ src/Request/AppRequest/RemovePermissionGroupForTableRequest.java
@@ -1,9 +1,7 @@
-/** 
- * FILE : RemovePermissionGroupForTableRequest.java
- * AUTHORS : Erez Gotlieb    
- * DESCRIPTION : 
- */ 
-
+/**
+ * FILE : RemovePermissionGroupForTableRequest.java AUTHORS : Erez Gotlieb
+ * DESCRIPTION :
+ */
 package Request.AppRequest;
 
 import Request.Credentials;
@@ -15,45 +13,45 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class RemovePermissionGroupForTableRequest extends AppRequest{
+public class RemovePermissionGroupForTableRequest extends AppRequest {
 
-    Permission toRemove;
-    
-    public RemovePermissionGroupForTableRequest(Permission.PERMISSION_TYPE type, String appName, String tableName, String gorupName, Credentials creds) {
-        super(creds);
-        this.toRemove = new Permission(type, appName, tableName, gorupName);
-    }
+	Permission toRemove;
 
-    @Override
-    public APP_ACTION_TYPE getActionType() {
-        return APP_ACTION_TYPE.REMOVE_PERMISSION_GROUP_FOR_TABLE;
-    }
+	public RemovePermissionGroupForTableRequest(Permission.PERMISSION_TYPE type, String appName, String tableName, String gorupName, Credentials creds) {
+		super(creds);
+		this.toRemove = new Permission(type, appName, appName + "_" + tableName, gorupName);
+	}
 
-    @Override
-    protected boolean CheckPermissions(SqlExecutor sqlExc) throws SQLException, ValidationException {
-        //check appName exists
-		if(!ExistenceValidator.isAppByName(sqlExc, this.toRemove.getAppName())){
+	@Override
+	public APP_ACTION_TYPE getActionType() {
+		return APP_ACTION_TYPE.REMOVE_PERMISSION_GROUP_FOR_TABLE;
+	}
+
+	@Override
+	protected boolean CheckPermissions(SqlExecutor sqlExc) throws SQLException, ValidationException {
+		//check appName exists
+		if (!ExistenceValidator.isAppByName(sqlExc, this.toRemove.getAppName())) {
 			throw new ValidationException(1);
 		}
-		
+
 		//check table name exists
-		if(!ExistenceValidator.isTableByName(sqlExc, this.toRemove.getTableName())){
+		if (!ExistenceValidator.isTableByName(sqlExc, this.toRemove.getTableName())) {
 			throw new ValidationException(12);
 		}
 		//check permission group exists
-		if(!ExistenceValidator.isPermissionGroupByName(sqlExc, this.toRemove.getPermissionGroup())){
+		if (!ExistenceValidator.isPermissionGroupByName(sqlExc, this.toRemove.getPermissionGroup())) {
 			throw new ValidationException(11);
 		}
-		
-        if(!this.creds.isAppSuperAdmin(this.toRemove.getAppName())){
+
+		if (!this.creds.isAppSuperAdmin(this.toRemove.getAppName())) {
 			throw new ValidationException(6);
 		}
 		return true;
-    }
+	}
 
-    @Override
-    protected ResultSet performRequest(SqlExecutor sqlExc) throws SQLException {
-        final String table_name = this.toRemove.getTableName();
+	@Override
+	protected ResultSet performRequest(SqlExecutor sqlExc) throws SQLException {
+		final String table_name = this.toRemove.getTableName();
 		final String group_name = this.toRemove.getPermissionGroup();
 		final String permission_type = this.toRemove.getType().toString();
 		sqlExc.executePreparedStatement("DeleteSingleAppPermission", new StatementPreparer() {
@@ -65,6 +63,5 @@ public class RemovePermissionGroupForTableRequest extends AppRequest{
 			}
 		});
 		return null;
-    }
-    
+	}
 }
